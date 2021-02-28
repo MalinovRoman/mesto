2cb8562b8758540f6da1fea74c11f17828d2d31e
MalinovRoman <romanmalinov@yandex.ru>
2021-02-28 23:36:29 +0300

    new html

---
+++ scr.js
@@ -0,0 +1,8 @@
+//alert( 'hello world' );
+document.querySelector('profile')
+let profile = document.querySelector('profile');
+const body = documetn.querySelector('body');
+console.dir(body);
+
+
+const titels = document.querySelectorAll('img')
