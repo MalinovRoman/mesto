ab9d907f3e534b1954489794192267c97501bc5d
MalinovRoman <romanmalinov@yandex.ru>
2021-03-13 19:00:25 +0300

    Создан черновой вариант сайта без БЭМ и JS

--- scr.js
+++
@@ -1,8 +0,0 @@
-//alert( 'hello world' );
-document.querySelector('profile')
-let profile = document.querySelector('profile');
-const body = documetn.querySelector('body');
-console.dir(body);
-
-
-const titels = document.querySelectorAll('img')
