79ee1dcc28335e051e94677ceb41a0b75efee248
MalinovRoman <romanmalinov@yandex.ru>
2021-03-18 01:05:45 +0300

    Исправление верстки, стилей, добавлена интерективность посредством js

---
+++ script/script.js
@@ -0,0 +1,49 @@
+// открытие и закрытие попапа
+let popup = document.querySelector('.popup');
+let openPopupBtn = document.querySelector('.profile__info-adit-button');
+let closePopupBtn = document.querySelector('.popup__button-close')
+let closePopupOverLay = document.querySelector('.popup');
+let popupConteiner = document.querySelector('.popup__conteiner');
+
+function openPopup() {
+    popup.classList.add('popup_visible');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_visible');
+}
+
+openPopupBtn.addEventListener('click', function () {
+    openPopup();
+});
+
+closePopupBtn.addEventListener('click', function () {
+    closePopup();
+});
+
+closePopupOverLay.addEventListener('click', function (event) {
+    closePopup();
+});
+
+popupConteiner.addEventListener('click', function (event) {
+    event.stopImmediatePropagation();
+});
+
+
+
+// Редактирование имени и информации о себе
+let textProfileSubtitel = document.querySelector('.profile__info-subtitle');
+let textProfiletitel = document.querySelector('.profile__info-title')
+let formElement = document.querySelector('.popup__form');
+let popupInputName = document.querySelector('.popup__input-name');
+let popupInputAbout = document.querySelector('.popup__input-about');
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    textProfileSubtitel.textContent = popupInputAbout.value; popupInputName.value;
+    textProfiletitel.textContent = popupInputName.value
+    closePopup()
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
